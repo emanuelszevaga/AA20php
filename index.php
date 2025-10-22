2e0f569660c7456f0667bf1551321c0b1922a2d0
Emanuel <emanuelszevaga7@gmail.com>
2025-10-22 20:37:12 -0300

    nuevas actividades

--- index.php
+++ index.php
@@ -50,7 +50,61 @@
                         <a href="assets\php\ejercicio06_formulario.php">
                             Ejercicio 6
                         </a>
+                    </li>
+                    
+                    <li>
+                        <a href="assets\php\ejercicio07.php">
+                            Ejercicio 7
+                        </a>
                     </li>  
+
+                    <li>
+                        <a href="assets\php\ejercicio08.php">
+                            Ejercicio 8
+                        </a>
+                    </li>  
+
+                    <li>
+                        <a href="assets\php\ejercicio09.php">
+                            Ejercicio 9
+                        </a>
+                    </li> 
+
+                    <li>
+                        <a href="assets\php\ejercicio10.php">
+                            Ejercicio 10
+                        </a>
+                    </li> 
+                    
+                    <li>
+                        <a href="assets\php\ejercicio11.php">
+                            Ejercicio 11
+                        </a>
+                    </li>
+                    
+                    <li>
+                        <a href="assets\php\ejercicio12.php">
+                            Ejercicio 12
+                        </a>
+                    </li>
+                    
+                    <li>
+                        <a href="assets\php\ejercicio13.php">
+                            Ejercicio 13
+                        </a>
+                    </li>  
+
+                    <li>
+                        <a href="assets\php\ejercicio14.php">
+                            Ejercicio 14
+                        </a>
+                    </li> 
+
+                    <li>
+                        <a href="assets\php\ejercicio15.php">
+                            Ejercicio 15
+                        </a>
+                    </li> 
                 </ul>
             </section>
         </div>
